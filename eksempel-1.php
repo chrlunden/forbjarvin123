74a0417ddaa6039b91388ddc823aa6c1e674bdaa
Christian Lunden <christian.lunden@usn.no>
2025-03-18 14:26:36 +0100

    tester igjen

--- eksempel-1.php
+++ eksempel-1.php
@@ -6,5 +6,5 @@
   $fornavn=$_POST ["fornavn"];
   $etternavn=$_POST ["etternavn"];  /* variable gitt verdier fra feltene i HTML-skjemaet */
 	
-  print ("God dag $fornavn $etternavn <br />");  /* resultat skrevet ut  */	
+  print ("God ettermiddag $fornavn $etternavn <br />");  /* resultat skrevet ut  */	
 ?>
