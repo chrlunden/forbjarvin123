86a29326a61ce14253e22bd3f1d64302291c1e9b
Christian Lunden <christian.lunden@usn.no>
2025-03-19 09:09:49 +0100

    tester!

---
+++ eksempel-15.php
@@ -0,0 +1,10 @@
+<?php    /* Eksempel 1 */
+/*
+/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+/*    Programmet skriver ut en "god dag"-melding med personens navn 
+*/
+  $fornavn=$_POST ["fornavn"];
+  $etternavn=$_POST ["etternavn"];  /* variable gitt verdier fra feltene i HTML-skjemaet */
+	
+  print ("God Ettermiddag $fornavn $etternavn <br />");  /* resultat skrevet ut  */	
+?>
