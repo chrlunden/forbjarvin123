087db3a089589eafecd65dd35e2292cb37b89459
Christian Lunden <christian.lunden@usn.no>
2025-03-24 11:08:43 +0100

    teste feil

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php    /* Eksempel 1 */
+/*
+/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+/*    Programmet skriver ut en "god dag"-melding med personens navn 
+*/
+  
+  $etternavn=$_POST ["etternavn"];  /* variable gitt verdier fra feltene i HTML-skjemaet */
+	
+  print ("God Dag $fornavn $etternavn <br />");  /* resultat skrevet ut  */	
+?>
